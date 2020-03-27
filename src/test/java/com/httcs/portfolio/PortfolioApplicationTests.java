80bc3538aa714b098bb9c672220b89bb6762256c
geisonfeitosa <geisonlealfeitosa.glf@gmail.com>
2020-03-27 10:03:27 -0300

    inicio backend

---
+++ src/test/java/com/httcs/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.httcs.portfolio;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
